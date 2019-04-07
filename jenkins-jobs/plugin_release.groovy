239ba6c9a8a7de6d449082d85038277d4a023f29
William Jobin <wjobin@mirego.com>
2019-04-07 15:59:12 -0400

    Add release jenkins job

---
+++ jenkins-jobs/plugin_release.groovy
@@ -0,0 +1,45 @@
+@GrabResolver('https://s3.amazonaws.com/mirego-maven/public')
+@Grab('com.mirego.jenkins:jenkins-jobs:1.2')
+import com.mirego.jenkins.jobs.Context
+
+Context context = Context.load(this)
+context.standardFolders()
+
+job(context.jobFullName) {
+    description("Make a release of kword-plugin")
+    logRotator(5)
+    scm {
+        git {
+            branch("${GIT_BRANCH}")
+            remote {
+                name('origin')
+                url("${GIT_URL}")
+                credentials('github')
+            }
+        }
+    }
+    wrappers {
+        credentialsBinding {
+            amazonWebServicesCredentialsBinding {
+                accessKeyVariable('MAVEN_AWS_KEY')
+                secretKeyVariable('MAVEN_AWS_SECRET')
+                credentialsId('mirego-maven-aws')
+            }
+        }
+    }
+    steps {
+        gradle {
+            useWrapper()
+            makeExecutable()
+            tasks(':kword-plugin:release')
+            switches('-i -s')
+        }
+    }
+    publishers {
+        slackNotifier {
+            notifyFailure(true)
+            notifyBackToNormal(true)
+            room(context.slackChannel)
+        }
+    }
+}
