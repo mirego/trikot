5537cffeee496928926283457a30cb3a70aab697
Martin Gagnon <mgagnon@mirego.com>
2019-05-30 13:26:33 -0400

    URL -> url

--- metaviews/src/commonMain/kotlin/com/mirego/trikot/metaviews/properties/SimpleImageFlow.kt
+++ metaviews/src/commonMain/kotlin/com/mirego/trikot/metaviews/properties/SimpleImageFlow.kt
@@ -8,7 +8,7 @@ open class SimpleImageFlow(
     override val imageResource: ImageResource? = null,
     override val tintColor: Color? = null,
     override val accessibilityText: String? = null,
-    override val URL: String? = null,
+    override val url: String? = null,
     override val onSuccess: Publisher<ImageFlow>? = null,
     override val onError: Publisher<ImageFlow>? = null
 ) : ImageFlow
