d41106997c5e604510b9a1049fca5a3cfabfaecb
Jean-David Tremblay <jdavid.tremblay@gmail.com>
2019-06-10 15:26:12 -0400

    Fix missing inheritance

--- metaviews/src/commonMain/kotlin/com/mirego/trikot/metaviews/text/RichTextTransform.kt
+++ metaviews/src/commonMain/kotlin/com/mirego/trikot/metaviews/text/RichTextTransform.kt
@@ -2,14 +2,13 @@ package com.mirego.trikot.metaviews.text
 
 import com.mirego.trikot.metaviews.resource.Font
 
-sealed class RichTextTransform {
-    class FontTransform(val font: Font) : RichTextTransform()
-    class StyleTransform(val style: Style) {
-        enum class Style {
-            NORMAL,
-            BOLD,
-            ITALIC,
-            UNDERLINE
-        }
+sealed class RichTextTransform
+class FontTransform(val font: Font) : RichTextTransform()
+class StyleTransform(val style: Style) : RichTextTransform() {
+    enum class Style {
+        NORMAL,
+        BOLD,
+        ITALIC,
+        UNDERLINE
     }
 }
