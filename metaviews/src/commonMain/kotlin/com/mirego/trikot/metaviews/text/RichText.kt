957c164a5527690c1c8b15ab3c75788ec3dd0a7b
Jean-David Tremblay <jdavid.tremblay@gmail.com>
2019-06-11 08:51:19 -0400

    Replace interface by data class for RichText (#6)

--- metaviews/src/commonMain/kotlin/com/mirego/trikot/metaviews/text/RichText.kt
+++ metaviews/src/commonMain/kotlin/com/mirego/trikot/metaviews/text/RichText.kt
@@ -1,6 +1,21 @@
 package com.mirego.trikot.metaviews.text
 
-interface RichText {
-    val text: String
-    val ranges: List<RichTextRange>
+import com.mirego.trikot.metaviews.resource.Font
+
+data class RichText(val text: String, val ranges: List<RichTextRange>)
+
+data class RichTextRange(val range: IntRange, val transform: RichTextTransform)
+
+sealed class RichTextTransform
+
+data class FontTransform(val font: Font) : RichTextTransform()
+
+data class StyleTransform(val style: Style) : RichTextTransform() {
+    enum class Style {
+        NORMAL,
+        BOLD,
+        ITALIC,
+        BOLD_ITALIC,
+        UNDERLINE
+    }
 }
--- metaviews/src/commonMain/kotlin/com/mirego/trikot/metaviews/text/RichTextRange.kt
+++
@@ -1,6 +0,0 @@
-package com.mirego.trikot.metaviews.text
-
-interface RichTextRange {
-    val range: IntRange
-    val transform: RichTextTransform
-}
