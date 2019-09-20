60b2cf86a8248bc826dbf7396502336dd13608db
Jean-David Tremblay <jdavid.tremblay@gmail.com>
2019-09-20 11:17:46 -0400

    Fix:ObserveOnProcessor dispatch event sequentially (#28)

--- streams/src/commonMain/kotlin/com/mirego/trikot/streams/reactive/processors/ObserveOnProcessor.kt
+++ streams/src/commonMain/kotlin/com/mirego/trikot/streams/reactive/processors/ObserveOnProcessor.kt
@@ -2,14 +2,21 @@ package com.mirego.trikot.streams.reactive.processors
 
 import com.mirego.trikot.foundation.concurrent.dispatchQueue.DispatchQueue
 import com.mirego.trikot.foundation.concurrent.dispatchQueue.QueueDispatcher
-import com.mirego.trikot.foundation.concurrent.freeze
+import com.mirego.trikot.foundation.concurrent.dispatchQueue.SequentialDispatchQueue
 import com.mirego.trikot.foundation.concurrent.dispatchQueue.dispatch
+import com.mirego.trikot.foundation.concurrent.freeze
 import org.reactivestreams.Publisher
 import org.reactivestreams.Subscriber
 
-open class ObserveOnProcessor<T>(parentPublisher: Publisher<T>, override val dispatchQueue: DispatchQueue) :
-    AbstractProcessor<T, T>(parentPublisher = parentPublisher),
-    QueueDispatcher {
+open class ObserveOnProcessor<T>(
+    parentPublisher: Publisher<T>,
+    dispatchQueue: DispatchQueue
+) : AbstractProcessor<T, T>(parentPublisher = parentPublisher) {
+
+    private val dispatchQueue = when {
+        dispatchQueue.isSerial() -> dispatchQueue
+        else -> SequentialDispatchQueue(dispatchQueue)
+    }
 
     override fun createSubscription(subscriber: Subscriber<in T>): ProcessorSubscription<T, T> {
         return ObserveOnProcessorSubscription(subscriber, dispatchQueue)
@@ -19,24 +26,19 @@ open class ObserveOnProcessor<T>(parentPublisher: Publisher<T>, override val dis
         s: Subscriber<in T>,
         override val dispatchQueue: DispatchQueue
     ) : ProcessorSubscription<T, T>(s), QueueDispatcher {
+
         override fun onNext(t: T, subscriber: Subscriber<in T>) {
             freeze(t)
-            dispatch {
-                subscriber.onNext(t)
-            }
+            dispatch { subscriber.onNext(t) }
         }
 
         override fun onError(t: Throwable) {
             freeze(t)
-            dispatch {
-                super.onError(t)
-            }
+            dispatch { super.onError(t) }
         }
 
         override fun onComplete() {
-            dispatch {
-                super.onComplete()
-            }
+            dispatch { super.onComplete() }
         }
     }
 }
