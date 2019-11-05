815aac98fd9f48b7a7c9dfe2891334fdb9da2604
Simon Gingras <thegingman@gmail.com>
2019-11-05 15:13:06 -0500

    Add ApplicationOctetStream constant to http

--- http/src/commonMain/kotlin/com/mirego/trikot/http/HTTPConsts.kt
+++ http/src/commonMain/kotlin/com/mirego/trikot/http/HTTPConsts.kt
@@ -3,3 +3,4 @@ package com.mirego.trikot.http
 const val ContentType = "Content-Type"
 
 const val ApplicationJSON = "application/json"
+const val ApplicationOctetStream = "application/octet-stream"
