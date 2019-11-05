116b6d31afc2bfde348da5040799b4fe912f3e3a
Simon Gingras <thegingman@gmail.com>
2019-11-05 14:55:43 -0500

    Add ByteArrayNativeUtils helper to convert byte array to nsdata (#10)

---
+++ foundation/src/nativeMain/kotlin/com/mirego/trikot/foundation/helpers/ByteArrayNativeUtils.kt
@@ -0,0 +1,30 @@
+package com.mirego.trikot.foundation.helpers
+
+import kotlinx.cinterop.addressOf
+import kotlinx.cinterop.allocArrayOf
+import kotlinx.cinterop.memScoped
+import kotlinx.cinterop.usePinned
+import platform.Foundation.NSData
+import platform.Foundation.create
+import platform.posix.memcpy
+
+object ByteArrayNativeUtils {
+    @ExperimentalUnsignedTypes
+    fun convert(data: NSData): ByteArray {
+        val memScopedData = memScoped { data }
+        val byteArray = ByteArray(memScopedData.length.toInt()).apply {
+            usePinned {
+                memcpy(it.addressOf(0), memScopedData.bytes, memScopedData.length)
+            }
+        }
+        return byteArray
+    }
+
+    @ExperimentalUnsignedTypes
+    fun convert(byteArray: ByteArray): NSData {
+        return memScoped {
+            NSData.create(bytes = allocArrayOf(byteArray),
+                length = byteArray.size.toULong())
+        }
+    }
+}
