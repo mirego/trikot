4a80f1f41af145cd202dd8aa8c8f5c039d7f1f7e
Martin Gagnon <mgagnon@mirego.com>
2019-11-12 13:33:34 -0500

    Add helper methods on DataSourceState

--- datasources/src/commonMain/kotlin/com/mirego/trikot/datasources/DataSourceState.kt
+++ datasources/src/commonMain/kotlin/com/mirego/trikot/datasources/DataSourceState.kt
@@ -1,3 +1,21 @@
 package com.mirego.trikot.datasources
 
-data class DataSourceState<T>(val isLoading: Boolean, val data: T?, val error: Throwable? = null)
+data class DataSourceState<T>(val isLoading: Boolean, val data: T?, val error: Throwable? = null) {
+    fun <R> mapData(lambda: (T?) -> R?): DataSourceState<R> {
+        return DataSourceState(isLoading, lambda(data), error)
+    }
+
+    companion object {
+        fun <T> dataLoaded(data: T): DataSourceState<T> {
+            return DataSourceState(false, data)
+        }
+
+        fun <T> loading(): DataSourceState<T> {
+            return DataSourceState(true, null)
+        }
+
+        fun <T> withError(error: Throwable): DataSourceState<T> {
+            return DataSourceState(false, null, error)
+        }
+    }
+}
