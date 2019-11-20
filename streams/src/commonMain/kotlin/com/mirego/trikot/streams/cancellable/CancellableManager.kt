961ed07befafb239a5b857f0ebf43e86a5ac299c
Jean-David Tremblay <jdavid.tremblay@gmail.com>
2019-11-20 14:48:04 -0500

    Ensure CancellableManager.cancel() is sequential (#43)

--- streams/src/commonMain/kotlin/com/mirego/trikot/streams/cancellable/CancellableManager.kt
+++ streams/src/commonMain/kotlin/com/mirego/trikot/streams/cancellable/CancellableManager.kt
@@ -2,8 +2,11 @@ package com.mirego.trikot.streams.cancellable
 
 import com.mirego.trikot.foundation.concurrent.AtomicListReference
 import com.mirego.trikot.foundation.concurrent.AtomicReference
+import com.mirego.trikot.foundation.concurrent.dispatchQueue.SynchronousSerialQueue
+import com.mirego.trikot.foundation.concurrent.freeze
 
 class CancellableManager : Cancellable {
+    private val serialQueue = freeze(SynchronousSerialQueue())
     private val queueList = AtomicListReference<Cancellable>()
     private val isCancelled = AtomicReference(false)
 
@@ -30,10 +33,12 @@ class CancellableManager : Cancellable {
     }
 
     private fun doCancelAll() {
-        val value = queueList.value
-        queueList.removeAll(value)
-        for (cancellable in value) {
-            cancellable.cancel()
+        serialQueue.dispatchQueue.dispatch {
+            val value = queueList.value
+            queueList.removeAll(value)
+            for (cancellable in value) {
+                cancellable.cancel()
+            }
         }
     }
 }
