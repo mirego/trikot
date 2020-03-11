98f645c5dc862c854e6f3c6c31d15faf1b09b972
Nicolas Presseault <npresseault@mirego.com>
2020-03-11 10:02:35 -0400

    Fix AndroidConnectivityPublisher not calling back when subscribing while offline (#17)
    Description
    Fixes an issue where we were not having any network state when device went offline after the ConnectivityPublisher was created.
    
    connectivityManager.registerNetworkCallback does not callback if the device is already offline when registering.
    
    Motivation and Context
    We were performing an http request which was silently not returning any errors due to the error handling using connectivity publisher.

--- android-ktx/src/main/java/com/mirego/trikot/http/android/ktx/AndroidConnectivityPublisher.kt
+++ android-ktx/src/main/java/com/mirego/trikot/http/android/ktx/AndroidConnectivityPublisher.kt
@@ -1,11 +1,11 @@
 package com.mirego.trikot.http.android.ktx
 
-import android.annotation.TargetApi
 import android.content.Context
 import android.content.ContextWrapper
 import android.net.ConnectivityManager
 import android.net.Network
 import android.net.NetworkCapabilities
+import android.net.NetworkInfo
 import android.net.NetworkRequest
 import android.os.Build
 import androidx.annotation.RequiresApi
@@ -19,6 +19,17 @@ class AndroidConnectivityPublisher(application: ContextWrapper) :
     private val connectivityManager: ConnectivityManager =
         application.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
+    private val activeNetworkState: ConnectivityState
+        get() {
+            return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+                    ?.asConnectivityState()
+            } else {
+                @Suppress("DEPRECATION")
+                connectivityManager.activeNetworkInfo?.asConnectivityState()
+            } ?: ConnectivityState.NONE
+        }
+
     private val networkRequest =
         NetworkRequest.Builder()
             .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
@@ -29,6 +40,7 @@ class AndroidConnectivityPublisher(application: ContextWrapper) :
         override fun onAvailable(network: Network) {
             super.onAvailable(network)
             value = connectivityManager.getNetworkCapabilities(network)?.asConnectivityState()
+                ?: ConnectivityState.NONE
         }
 
         override fun onCapabilitiesChanged(
@@ -47,12 +59,12 @@ class AndroidConnectivityPublisher(application: ContextWrapper) :
     }
 
     init {
-        val isConnected = connectivityManager.activeNetworkInfo?.isConnected == true
-        if (!isConnected) value = ConnectivityState.NONE
+        value = activeNetworkState
     }
 
     override fun onFirstSubscription() {
         super.onFirstSubscription()
+        value = activeNetworkState
         connectivityManager.registerNetworkCallback(networkRequest, networkCallback)
     }
 
@@ -63,11 +75,17 @@ class AndroidConnectivityPublisher(application: ContextWrapper) :
 }
 
 @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
-private fun NetworkCapabilities.asConnectivityState(): ConnectivityState {
-    return when {
-        hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
-            hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)
-            || hasTransport(NetworkCapabilities.TRANSPORT_VPN) -> ConnectivityState.WIFI
-        else -> ConnectivityState.CELLULAR
-    }
+private fun NetworkCapabilities.asConnectivityState() = when {
+    hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+        hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)
+        || hasTransport(NetworkCapabilities.TRANSPORT_VPN) -> ConnectivityState.WIFI
+    else -> ConnectivityState.CELLULAR
+}
+
+@Suppress("DEPRECATION")
+private fun NetworkInfo.asConnectivityState() = when (type) {
+    ConnectivityManager.TYPE_MOBILE -> ConnectivityState.CELLULAR
+    ConnectivityManager.TYPE_WIFI -> ConnectivityState.WIFI
+    ConnectivityManager.TYPE_ETHERNET -> ConnectivityState.WIFI
+    else -> ConnectivityState.NONE
 }
