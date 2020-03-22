be415fb2bcca9d7a093ea13a05d51e4095acd959
Nicolas Presseault <npresseault@mirego.com>
2020-03-22 15:40:20 -0400

    Make data state implementations data classes so we can call equals and hashcode on them.

--- datasources/src/commonMain/kotlin/com/mirego/trikot/datasources/DataState.kt
+++ datasources/src/commonMain/kotlin/com/mirego/trikot/datasources/DataState.kt
@@ -1,9 +1,24 @@
 package com.mirego.trikot.datasources
 
+@Suppress("EqualsOrHashCode")
 sealed class DataState<V, E : Throwable> {
-    class Pending<V, E : Throwable>(val value: V? = null) : DataState<V, E>()
-    class Data<V, E : Throwable>(val value: V) : DataState<V, E>()
-    class Error<V, E : Throwable>(val error: E, val value: V? = null) : DataState<V, E>()
+    data class Pending<V, E : Throwable>(val value: V? = null) : DataState<V, E>() {
+        override fun hashCode() = disambiguatedHashCode()
+    }
+
+    data class Data<V, E : Throwable>(val value: V) : DataState<V, E>() {
+        override fun hashCode() = disambiguatedHashCode()
+    }
+
+    data class Error<V, E : Throwable>(val error: E, val value: V? = null) : DataState<V, E>() {
+        override fun hashCode() = disambiguatedHashCode()
+    }
+
+    /**
+     * As opposed to equals, sealed class implementation with identical properties
+     * can share the same hashCode, this circumvents it
+     */
+    fun disambiguatedHashCode() = 31 * this::class.hashCode() + super.hashCode()
 
     fun isPending(pendingIfDataIsAvailable: Boolean = true): Boolean {
         return when (val result = this) {
