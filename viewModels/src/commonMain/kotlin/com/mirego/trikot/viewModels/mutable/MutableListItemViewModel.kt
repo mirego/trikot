6d64fc73f17a767ba111221752b6440c269e3e32
Nicolas Presseault <npresseault@mirego.com>
2020-07-16 14:47:23 -0400

    Checkstyles

--- viewModels/src/commonMain/kotlin/com/mirego/trikot/viewModels/mutable/MutableListItemViewModel.kt
+++ viewModels/src/commonMain/kotlin/com/mirego/trikot/viewModels/mutable/MutableListItemViewModel.kt
@@ -1,7 +1,6 @@
 package com.mirego.trikot.viewmodels.mutable
 
 import com.mirego.trikot.viewmodels.ListItemViewModel
-import com.mirego.trikot.viewmodels.mutable.MutableViewModel
 
 open class MutableListItemViewModel : MutableViewModel(), ListItemViewModel {
     override var comparableId = this.hashCode().toString()
