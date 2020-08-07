823cc5d1e56c1497c03029c7e02bdf2105e772c6
Martin Gagnon <mgagnon@mirego.com>
2020-08-06 20:52:13 -0400

    Fix multiple completion dispatched by the first processor (#65)

--- streams/src/commonMain/kotlin/com/mirego/trikot/streams/reactive/processors/FirstProcessor.kt
+++ streams/src/commonMain/kotlin/com/mirego/trikot/streams/reactive/processors/FirstProcessor.kt
@@ -1,5 +1,6 @@
 package com.mirego.trikot.streams.reactive.processors
 
+import com.mirego.trikot.foundation.concurrent.AtomicReference
 import org.reactivestreams.Publisher
 import org.reactivestreams.Subscriber
 
@@ -11,12 +12,19 @@ class FirstProcessor<T>(parentPublisher: Publisher<T>) :
     }
 
     class FirstProcessorSubscription<T>(s: Subscriber<in T>) : ProcessorSubscription<T, T>(s) {
-
+        private val hasReceivedValue = AtomicReference(false)
         override fun onNext(t: T, subscriber: Subscriber<in T>) {
             if (t != null) {
+                hasReceivedValue.compareAndSet(false, true)
                 cancelActiveSubscription()
                 subscriber.onNext(t)
                 subscriber.onComplete()
+            }
+        }
+
+        override fun onComplete() {
+            if (!hasReceivedValue.value) {
+                super.onComplete()
             }
         }
     }
--- streams/src/commonTest/kotlin/com/mirego/trikot/streams/reactive/processors/FirstProcessorTests.kt
+++ streams/src/commonTest/kotlin/com/mirego/trikot/streams/reactive/processors/FirstProcessorTests.kt
@@ -3,6 +3,8 @@ package com.mirego.trikot.streams.reactive.processors
 import com.mirego.trikot.streams.cancellable.CancellableManager
 import com.mirego.trikot.streams.reactive.Publishers
 import com.mirego.trikot.streams.reactive.first
+import com.mirego.trikot.streams.reactive.map
+import com.mirego.trikot.streams.reactive.shared
 import com.mirego.trikot.streams.reactive.subscribe
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -22,5 +24,19 @@ class FirstProcessorTests {
 
         assertEquals("a", value)
         assertTrue { completed }
+    }
+
+    @Test
+    fun firstProcessorDoesNotForwardCompletionIfCompleted() {
+        val initialPublisher = Publishers.behaviorSubject<String>()
+        val publisher1 = initialPublisher.first()
+        val publisher2 = publisher1.map { it }.first().shared()
+
+        var result = ""
+        publisher2.subscribe(CancellableManager()) {
+            result += it
+        }
+        initialPublisher.value = "EXPECTED"
+        assertEquals("EXPECTED", result)
     }
 }
