56b9237095a46bf33557b9e124e88c3e7a7194e6
Martin Gagnon <mgagnon@mirego.com>
2020-09-10 09:24:33 -0400

    Fix IOSSerialDispatchQueue (#29)

--- trikotFoundation/src/iosMain/kotlin/com/mirego/trikot/foundation/concurrent/dispatchQueue/IOSSerialDispatchQueue.kt
+++ trikotFoundation/src/iosMain/kotlin/com/mirego/trikot/foundation/concurrent/dispatchQueue/IOSSerialDispatchQueue.kt
@@ -1,14 +1,15 @@
 package com.mirego.trikot.foundation.concurrent.dispatchQueue
 
 import com.mirego.trikot.foundation.concurrent.freeze
+import platform.darwin.DISPATCH_QUEUE_SERIAL
 import platform.darwin.dispatch_async
 import platform.darwin.dispatch_queue_create
-import platform.darwin.dispatch_queue_serial_t
+import platform.darwin.dispatch_queue_t
 
 open class IOSSerialDispatchQueue(identifier: String) : DispatchQueue {
     private val serialQueue = dispatch_queue_create(
         "com.mirego.trikot.foundation.serial_dispatch_queue.$identifier",
-        dispatch_queue_serial_t()
+        DISPATCH_QUEUE_SERIAL as dispatch_queue_t
     )
 
     override fun isSerial() = true
