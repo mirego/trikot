5832e333eca9a55219cefaaf7c2382edbcb2f5dd
Martin Gagnon <mgagnon@mirego.com>
2020-09-10 11:21:58 -0400

    Upgrade to Kotlin 1.4 (#9)

--- analytics/src/commonMain/kotlin/com/mirego/trikot/analytics/TrackableViewModelAction.kt
+++ analytics/src/commonMain/kotlin/com/mirego/trikot/analytics/TrackableViewModelAction.kt
@@ -1,8 +1,8 @@
 package com.mirego.trikot.analytics
 
+import com.mirego.trikot.streams.reactive.just
 import com.mirego.trikot.viewmodels.properties.ViewModelAction
 import com.mirego.trikot.viewmodels.properties.ViewModelActionBlock
-import com.mirego.trikot.streams.reactive.just
 import org.reactivestreams.Publisher
 
 class TrackableViewModelAction(private val event: AnalyticsEvent, private val properties: Publisher<AnalyticsPropertiesType> = mapOf<String, Any>().just(), actionBlock: ViewModelActionBlock) : ViewModelAction(actionBlock) {
