cd4f922dded32da441cfb6618c18ce08b70f7241
Marc-Antoine Fortier <mafortier@mirego.com>
2020-11-26 10:33:50 -0500

    Use proper iOS/tvOS kotlin sourcesets configuration in build.gradle
    Current build fails resolve ios references at the moment. This sourceset configuration was suspicious.
    
    Also fix tvOS complaining about an Optional being null in NativeStringExtensions...

--- trikotFoundation/src/iosMain/kotlin/com/mirego/trikot/foundation/strings/NativeStringExtensions.kt
+++ trikotFoundation/src/iosMain/kotlin/com/mirego/trikot/foundation/strings/NativeStringExtensions.kt
@@ -1,10 +1,12 @@
 package com.mirego.trikot.foundation.strings
 
 import platform.Foundation.NSDiacriticInsensitiveSearch
+import platform.Foundation.NSLocale
 import platform.Foundation.NSString
 import platform.Foundation.create
 import platform.Foundation.stringByFoldingWithOptions
 
-actual fun String.normalize(): String =
-    NSString.create(string = this)
-        .stringByFoldingWithOptions(options = NSDiacriticInsensitiveSearch, locale = null)
+actual fun String.normalize(): String {
+    return NSString.create(string = this)
+        .stringByFoldingWithOptions(options = NSDiacriticInsensitiveSearch, locale = NSLocale.new())
+}
