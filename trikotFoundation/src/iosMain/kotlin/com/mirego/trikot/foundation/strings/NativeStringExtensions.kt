aebc32da9c7d61a8b7f90245e81342dfc0072c0c
Marc-Antoine Fortier <mafortier@mirego.com>
2020-11-26 12:29:12 -0500

    Use NSLocale.currentLocale when normalizing instead null
    tvos is complaining about a NSLocale? being null for some reason...

--- trikotFoundation/src/iosMain/kotlin/com/mirego/trikot/foundation/strings/NativeStringExtensions.kt
+++ trikotFoundation/src/iosMain/kotlin/com/mirego/trikot/foundation/strings/NativeStringExtensions.kt
@@ -4,9 +4,10 @@ import platform.Foundation.NSDiacriticInsensitiveSearch
 import platform.Foundation.NSLocale
 import platform.Foundation.NSString
 import platform.Foundation.create
+import platform.Foundation.currentLocale
 import platform.Foundation.stringByFoldingWithOptions
 
 actual fun String.normalize(): String {
     return NSString.create(string = this)
-        .stringByFoldingWithOptions(options = NSDiacriticInsensitiveSearch, locale = NSLocale.new())
+        .stringByFoldingWithOptions(options = NSDiacriticInsensitiveSearch, locale = NSLocale.currentLocale)
 }
