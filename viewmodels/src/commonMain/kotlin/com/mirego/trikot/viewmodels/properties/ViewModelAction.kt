81181f0693f97220924917896e9586c8edd4d57a
Mathieu Dumont <dumont.m@icloud.com>
2021-02-03 11:52:39 -0500

    Add JS IR compiler support (#74)

--- viewmodels/src/commonMain/kotlin/com/mirego/trikot/viewmodels/properties/ViewModelAction.kt
+++ viewmodels/src/commonMain/kotlin/com/mirego/trikot/viewmodels/properties/ViewModelAction.kt
@@ -1,14 +1,16 @@
 package com.mirego.trikot.viewmodels.properties
 
 import com.mirego.trikot.foundation.concurrent.freeze
+import kotlin.js.JsName
 
 typealias ViewModelActionBlock = (actionContext: Any?) -> Unit
 
 open class ViewModelAction(private var action: ViewModelActionBlock) {
     fun execute() {
-        execute(null)
+        action(null)
     }
 
+    @JsName("executeWithActionContext")
     open fun execute(actionContext: Any? = null) {
         action(actionContext)
     }
