9e172259b2944ce47d736d5b93e3cad747a2067d
Antoine Lamy <antoinelamy@gmail.com>
2021-03-01 10:55:40 -0500

    Update Kotlin to 1.4.30 (#15)
    * Remove deprecated jcenter
    
    * Update Kotlin to version 1.4.30
    
    * Use JDK 11 on CI
    
    * Use JS IR
    
    * Migrate Gradle scripts from Groovy to Kotlin DSL
    
    * Bump version number

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+buildscript {
+    repositories {
+        google()
+        mavenLocal()
+        maven("https://plugins.gradle.org/m2/")
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:4.1.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${project.extra["kotlin_version"]}")
+        classpath("org.jlleitschuh.gradle:ktlint-gradle:9.4.1")
+    }
+}
+
+repositories {
+    google()
+}
+
+allprojects {
+    repositories {
+        google()
+    }
+}
