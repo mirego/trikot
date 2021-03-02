1cec6ade4451ac7884e061390ef21b1c7c7173b4
Marc Lefrancois <mlefrancois@mirego.com>
2021-03-02 11:23:46 -0500

    Add a jenkins job so we can build from the legacy 1.0.x branch (#78)
    * Add a jenkins job so we can build from the legacy 1.0.x branch
    
    * Switch to branch parameter

--- jenkins-jobs/plugin_release.groovy
+++ jenkins-jobs/plugin_release.groovy
@@ -9,9 +9,17 @@ context.standardFolders()
 job(context.jobFullName) {
     description("Make a release of trikot.viewmodels")
     logRotator(5)
+    parameters {
+        stringParam {
+            name('Branch')
+            defaultValue("${GIT_BRANCH}")
+            description('The git branch to be built')
+            trim(true)
+        }
+    }
     scm {
         git {
-            branch("${GIT_BRANCH}")
+            branch("${Branch}")
             remote {
                 name('origin')
                 url("${GIT_URL}")
@@ -33,6 +41,53 @@ job(context.jobFullName) {
             useWrapper()
             makeExecutable()
             tasks(':viewmodels:release')
+            switches('-i -s')
+        }
+    }
+    publishers {
+        slackNotifier {
+            notifyFailure(true)
+            notifyBackToNormal(true)
+            room(context.slackChannel)
+        }
+    }
+}
+
+job("$context.jobFullName-android-ktx-1_0_*") {
+    description("Make a release of trikot.viewmodels:android-ktx")
+    logRotator(5)
+    parameters {
+        stringParam {
+            name('Branch')
+            defaultValue("release/1.0.x")
+            description('The git branch to be built')
+            trim(true)
+        }
+    }
+    scm {
+        git {
+            branch("${Branch}")
+            remote {
+                name('origin')
+                url("${GIT_URL}")
+                credentials('github')
+            }
+        }
+    }
+    wrappers {
+        credentialsBinding {
+            amazonWebServicesCredentialsBinding {
+                accessKeyVariable('MAVEN_AWS_KEY')
+                secretKeyVariable('MAVEN_AWS_SECRET')
+                credentialsId('mirego-maven-aws')
+            }
+        }
+    }
+    steps {
+        gradle {
+            useWrapper()
+            makeExecutable()
+            tasks(':android-ktx:release')
             switches('-i -s')
         }
     }
