6830fddf616d765bb58f5e9d4a61729e244c5a5c
Antoine Lamy <antoinelamy@gmail.com>
2021-06-08 15:38:13 -0400

    Update Kotlin to version 1.5.10 and add watchOS and macOS targets (#29)

--- datasources/build.gradle.kts
+++ datasources/build.gradle.kts
@@ -26,6 +26,8 @@ kotlin {
     ios()
     iosArm32("iosArm32")
     tvos()
+    watchos()
+    macosX64()
     js(IR) {
         browser()
     }
@@ -99,6 +101,22 @@ kotlin {
         }
 
         val tvosX64Main by getting {
+            dependsOn(nativeMain)
+        }
+
+        val watchos32Main by creating {
+            dependsOn(nativeMain)
+        }
+
+        val watchosArm64Main by getting {
+            dependsOn(nativeMain)
+        }
+
+        val watchosX64Main by getting {
+            dependsOn(nativeMain)
+        }
+
+        val macosX64Main by getting {
             dependsOn(nativeMain)
         }
     }
