c8e5f64961e48392184ee81c38d8026b81ed858a
Jean-Philippe Martin <jpmartin@mirego.com>
2021-11-01 09:48:18 -0400

    Upgrade Kotlin, Gradle, Trikot.streams, Trikot.foundation (#14)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to the library will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
+## 2.3.0
+> 2021-11-01
+- Updated Kotlin version to `1.5.31`
+
 ## 2.2.0
 > 2021-06-10
 - Added watchOS and macOS targets
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,14 @@ buildscript {
     repositories {
         google()
         mavenLocal()
+        mavenCentral()
         maven("https://plugins.gradle.org/m2/")
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:4.1.3")
+        classpath("com.android.tools.build:gradle:4.2.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${project.extra["kotlin_version"]}")
-        classpath("org.jlleitschuh.gradle:ktlint-gradle:9.4.1")
+        classpath("org.jlleitschuh.gradle:ktlint-gradle:10.2.0")
     }
 }
 
@@ -19,5 +20,6 @@ repositories {
 allprojects {
     repositories {
         google()
+        mavenCentral()
     }
 }
