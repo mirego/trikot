21a9e33826508b6c925b8715a7b6c70ed3588f96
Jean-Philippe Martin <jpmartin@mirego.com>
2021-11-01 10:04:16 -0400

    Update Kotlin, Gradle, Trikot.streams, Trikot.foundation (#112)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ buildscript {
     repositories {
         google()
         mavenLocal()
+        mavenCentral()
         maven("https://plugins.gradle.org/m2/")
     }
 
@@ -9,7 +10,7 @@ buildscript {
         classpath("com.android.tools.build:gradle:4.1.3")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${project.extra["kotlin_version"]}")
         classpath("org.jetbrains.kotlin:kotlin-serialization:${project.extra["kotlin_version"]}")
-        classpath("org.jlleitschuh.gradle:ktlint-gradle:9.4.1")
+        classpath("org.jlleitschuh.gradle:ktlint-gradle:10.2.0")
     }
 }
 
@@ -20,5 +21,6 @@ repositories {
 allprojects {
     repositories {
         google()
+        mavenCentral()
     }
 }
