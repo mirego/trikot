6a624139cd065b50b5786020e6615afd14161761
Nicolas Presseault <npresseault@mirego.com>
2021-11-10 13:12:54 -0500

    Bump to Java 11

--- viewmodels/build.gradle.kts
+++ viewmodels/build.gradle.kts
@@ -158,8 +158,8 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility(JavaVersion.VERSION_1_8)
-        targetCompatibility(JavaVersion.VERSION_1_8)
+        sourceCompatibility(JavaVersion.VERSION_11)
+        targetCompatibility(JavaVersion.VERSION_11)
     }
 
     sourceSets {
@@ -179,7 +179,7 @@ android {
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_1_8.toString()
+        jvmTarget = JavaVersion.VERSION_11.toString()
     }
 }
 
