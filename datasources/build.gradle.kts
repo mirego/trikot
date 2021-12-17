428f0df5b72ef1cd2165f24a100a68a72ff941be
Hugo Lefrancois <hlefrancois@mirego.com>
2021-12-17 17:41:28 -0500

    Add support for Apple iOS simulator on Apple Silicon platforms (#33)

--- datasources/build.gradle.kts
+++ datasources/build.gradle.kts
@@ -24,6 +24,7 @@ kotlin {
     jvm()
     ios()
     iosArm32("iosArm32")
+    iosSimulatorArm64()
     tvos()
     watchos()
     macosX64()
@@ -89,6 +90,10 @@ kotlin {
 
         val iosArm64Main by getting {
             dependsOn(nativeMain)
+        }
+
+        val iosSimulatorArm64Main by getting {
+            dependsOn(iosArm64Main)
         }
 
         val iosX64Main by getting {
