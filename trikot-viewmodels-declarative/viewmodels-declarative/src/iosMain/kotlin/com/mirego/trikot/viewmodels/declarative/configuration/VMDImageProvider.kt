ff6e7a756edf3b66b399f7e057b277f552e1d8fb
Antoine Lamy <antoinelamy@gmail.com>
2022-02-11 16:39:08 -0500

    Use UIImage as VMDImageProvider return type

--- trikot-viewmodels-declarative/viewmodels-declarative/src/iosMain/kotlin/com/mirego/trikot/viewmodels/declarative/configuration/VMDImageProvider.kt
+++ trikot-viewmodels-declarative/viewmodels-declarative/src/iosMain/kotlin/com/mirego/trikot/viewmodels/declarative/configuration/VMDImageProvider.kt
@@ -1,7 +1,8 @@
 package com.mirego.trikot.viewmodels.declarative.configuration
 
 import com.mirego.trikot.viewmodels.declarative.properties.VMDImageResource
+import platform.UIKit.UIImage
 
 actual interface VMDImageProvider {
-    fun imageNameForResource(imageResource: VMDImageResource): String?
+    fun imageNameForResource(imageResource: VMDImageResource): UIImage?
 }
