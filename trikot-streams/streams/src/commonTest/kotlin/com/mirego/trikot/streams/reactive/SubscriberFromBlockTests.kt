9b43801d741c9b46288d979a371523b8d831caf2
Nicolas Presseault <npresseault@mirego.com>
2022-02-15 15:27:56 -0500

    This fixes an issue where request would never be called on subscribe request is usually used for backpressure management which we don't support but to be compliant with the spec, we must still request for Long.MAX_VALUE. It marks the Publisher as "effectively unbounded".
    This fix was required to be able to use coroutines interop https://github.com/Kotlin/kotlinx.coroutines/tree/master/reactive/kotlinx-coroutines-reactive

--- trikot-streams/streams/src/commonTest/kotlin/com/mirego/trikot/streams/reactive/SubscriberFromBlockTests.kt
+++ trikot-streams/streams/src/commonTest/kotlin/com/mirego/trikot/streams/reactive/SubscriberFromBlockTests.kt
@@ -5,8 +5,10 @@ import com.mirego.trikot.streams.reactive.processors.AbstractProcessor
 import com.mirego.trikot.streams.reactive.processors.ProcessorSubscription
 import org.reactivestreams.Publisher
 import org.reactivestreams.Subscriber
+import org.reactivestreams.Subscription
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class SubscriberFromBlockTests {
     var behaviorSubject = Publishers.behaviorSubject<String>()
@@ -29,6 +31,30 @@ class SubscriberFromBlockTests {
         }
 
         assertEquals(1, count)
+    }
+
+    @Test
+    fun whenSubscribingItRequestAllEvents() {
+        val cancellableManager = CancellableManager()
+        var requestCount = 0L
+
+        val publisher = object : Publisher<Int> {
+            override fun subscribe(s: Subscriber<in Int>) {
+                s.onSubscribe(object: Subscription {
+                    override fun request(n: Long) {
+                        requestCount = n
+                    }
+
+                    override fun cancel() {
+                    }
+                })
+            }
+        }
+        publisher.subscribe(cancellableManager) {
+
+        }
+
+        assertEquals(Long.MAX_VALUE, requestCount)
     }
 
     class TriggerHappyProcessor<T>(parentPublisher: Publisher<T>, private val value: T) :
