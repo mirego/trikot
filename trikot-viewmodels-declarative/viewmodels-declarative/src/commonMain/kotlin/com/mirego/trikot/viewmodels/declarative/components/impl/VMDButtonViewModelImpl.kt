4257e551b747ab5c44232e385d0ada35e9353a8e
Antoine Lamy <antoinelamy@gmail.com>
2022-03-03 09:44:41 -0500

    [VMD] Refactor VMDButtonViewModel action block to remove unnecessary overhead (#37)
    * Refactor VMDButtonViewModel action block to remove unecessary overhead
    
    * Remove unused imports

--- trikot-viewmodels-declarative/viewmodels-declarative/src/commonMain/kotlin/com/mirego/trikot/viewmodels/declarative/components/impl/VMDButtonViewModelImpl.kt
+++ trikot-viewmodels-declarative/viewmodels-declarative/src/commonMain/kotlin/com/mirego/trikot/viewmodels/declarative/components/impl/VMDButtonViewModelImpl.kt
@@ -1,11 +1,9 @@
 package com.mirego.trikot.viewmodels.declarative.components.impl
 
 import com.mirego.trikot.streams.cancellable.CancellableManager
-import com.mirego.trikot.streams.reactive.PublishSubjectImpl
 import com.mirego.trikot.streams.reactive.first
 import com.mirego.trikot.streams.reactive.observeOn
 import com.mirego.trikot.streams.reactive.subscribe
-import com.mirego.trikot.streams.reactive.switchMap
 import com.mirego.trikot.viewmodels.declarative.components.VMDButtonViewModel
 import com.mirego.trikot.viewmodels.declarative.content.VMDContent
 import com.mirego.trikot.viewmodels.declarative.utilities.VMDDispatchQueues
@@ -19,37 +17,26 @@ open class VMDButtonViewModelImpl<C : VMDContent>(
     defaultContent: C
 ) : VMDButtonViewModel<C>(cancellableManager) {
 
-    private val actionPublisher = PublishSubjectImpl<Unit>()
-    override val actionBlock: () -> Unit = {
-        actionPublisher.value = Unit
-    }
+    override var actionBlock: () -> Unit = {}
 
     private val contentDelegate = published(defaultContent, this)
     override var content: C by contentDelegate
 
     fun setAction(action: () -> Unit) {
-        actionPublisher
-            .observeOn(VMDDispatchQueues.uiQueue)
-            .subscribe(
-                cancellableManager,
-                onNext = {
-                    action()
-                }
-            )
+        actionBlock = {
+            VMDDispatchQueues.uiQueue.dispatch(action)
+        }
     }
 
     fun <T> setAction(publisher: Publisher<T>, action: (T) -> Unit) {
-        actionPublisher
-            .switchMap {
-                publisher.first()
-            }
-            .observeOn(VMDDispatchQueues.uiQueue)
-            .subscribe(
-                cancellableManager,
-                onNext = {
+        actionBlock = {
+            publisher
+                .first()
+                .observeOn(VMDDispatchQueues.uiQueue)
+                .subscribe(cancellableManager, onNext = {
                     action(it)
-                }
-            )
+                })
+        }
     }
 
     fun bindContent(publisher: Publisher<C>) {
