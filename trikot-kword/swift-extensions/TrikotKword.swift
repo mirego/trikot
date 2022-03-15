d989f467b6cd6a0cee4d1ce87d3633da089fa6ef
Nicolas Presseault <npresseault@mirego.com>
2022-03-15 09:02:00 -0400

    WIP

--- trikot-kword/swift-extensions/TrikotKword.swift
+++ trikot-kword/swift-extensions/TrikotKword.swift
@@ -3,24 +3,23 @@ import Trikot
 import TRIKOT_FRAMEWORK_NAME
 
 public class TrikotKword: NSObject {
-    private static let translationBasePaths = [
+    public static let translationBasePaths = [
         "translation"
     ]
     
     public static let shared = TrikotKword()
 
-    public func setCurrentLanguage(_ languageCode: String) {
-        KwordLoader.shared.setCurrentLanguageCode(code: languageCode, basePaths: TrikotKword.translationBasePaths)
+    public func setCurrentLanguage(_ languageCode: String, basePaths: [String] = TrikotKword.translationBasePaths) {
+        setCurrentLanguage(i18N: KWord.shared, basePaths: basePaths, codes: languageCode)
     }
 
-    public func setCurrentLanguage(i18N: I18N, languageCode: String) {
-        KwordLoader.shared.setCurrentLanguageCode(i18N: i18N, basePaths: TrikotKword.translationBasePaths, code: languageCode)
+    public func setCurrentLanguage(i18N: I18N, languageCode: String, basePaths: [String] = TrikotKword.translationBasePaths) {
+        setCurrentLanguage(i18N: i18N, basePaths: basePaths, codes: languageCode)
     }
 
-    public func setCurrentLanguage(i18N: I18N, codes: String...) {
-        KwordLoader.shared.setCurrentLanguageCodes(i18N: i18N, basePaths: TrikotKword.translationBasePaths, codes: KotlinArray<NSString>(size: Int32(codes.count)) { index in
+    public func setCurrentLanguage(i18N: I18N, basePaths: [String] = TrikotKword.translationBasePaths, codes: String...) {
+        KwordLoader.shared.setCurrentLanguageCodes(i18N: i18N, basePaths: basePaths, codes: KotlinArray<NSString>(size: Int32(codes.count)) { index in
             codes[index.intValue] as NSString
         })
     }
 }
-
