b424bb12dcb403f98333b449dc0b10d5aaa89602
Philippe Lamy <71347410+philippelamy1@users.noreply.github.com>
2022-06-17 09:29:04 -0400

    [VMD] Format, unformat and transform text (#74)
    * Add unformatText to viewmodel-declarative
    
    * Add transformText
    
    * Revert version, modify VMDTextField
    
    * ignore podspec for sample of kword and vmd
    
    * Remove reference to podspecs

--- trikot-viewmodels-declarative/swift/swiftui/Components/VMDTextField.swift
+++ trikot-viewmodels-declarative/swift/swiftui/Components/VMDTextField.swift
@@ -12,13 +12,7 @@ public struct VMDTextField<Label>: View where Label: View {
         observableViewModel.viewModel
     }
 
-    private var text: Binding<String> {
-        Binding {
-            self.viewModel.text
-        } set: { text in
-            self.viewModel.onValueChange(text: self.viewModel.formatText(text))
-        }
-    }
+    @State private var text: String = ""
 
     private var prompt: Text? {
         if !viewModel.placeholder.isEmpty {
@@ -81,6 +75,12 @@ public struct VMDTextField<Label>: View where Label: View {
                     }
                 }
             }
+            .onChange(of: viewModel.text) { newValue in
+                text = viewModel.formatText(newValue)
+            }
+            .onChange(of: text) { newValue in
+                handleTextTransformations(newValue)
+            }
         } else {
             TextField(viewModel.placeholder, text: text, onEditingChanged: { isEditing in
                 self.onFocusChange?(isEditing)
@@ -94,6 +94,18 @@ public struct VMDTextField<Label>: View where Label: View {
                 .textContentType(viewModel.contentType?.uiTextContentType)
                 .autocapitalization(viewModel.autoCapitalization.uiTextAutocapitalizationType)
                 .disableAutocorrection(!viewModel.autoCorrect)
+                .onChange(of: viewModel.text) { newValue in
+                    text = viewModel.formatText(newValue)
+                }
+                .onChange(of: text) { newValue in
+                    handleTextTransformations(newValue)
+                }
         }
+    }
+
+    private func handleTextTransformations(_ value: String) {
+        let unformattedText = viewModel.unformatText(value)
+        text = viewModel.formatText(viewModel.transformText(unformattedText))
+        viewModel.onValueChange(text: unformattedText)
     }
 }
