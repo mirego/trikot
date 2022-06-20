de3420a1b3fde11e16df7eef39dac6290b8fe7d6
Nicolas Presseault <npresseault@mirego.com>
2022-06-20 12:03:39 -0400

    Use debug logging

--- jenkins-jobs/release.groovy
+++ jenkins-jobs/release.groovy
@@ -41,7 +41,7 @@ job(context.jobFullName) {
           source "${SDKMAN_DIR}/bin/sdkman-init.sh"
           sdk install java 11.0.10.hs-adpt
           sdk use java 11.0.10.hs-adpt
-          ./gradlew release -i -s''')
+          ./gradlew release -d -s''')
     }
     publishers {
         slackNotifier {
