9b00411e2d05ba81c8759519ba6039d45f3dac9a
Steven de Tilly <steven.detilly@gmail.com>
2022-06-22 10:58:29 -0400

    PR Comments

--- trikot-viewmodels-declarative/compose/src/main/kotlin/com/mirego/trikot/viewmodels/declarative/compose/extensions/ViewModelExtensions.kt
+++ trikot-viewmodels-declarative/compose/src/main/kotlin/com/mirego/trikot/viewmodels/declarative/compose/extensions/ViewModelExtensions.kt
@@ -1,6 +1,5 @@
 package com.mirego.trikot.viewmodels.declarative.compose.extensions
 
-import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.State
@@ -50,17 +49,14 @@ fun <VM : VMDViewModel, T> VM.observeAnimatedPropertyAsState(
     val propertyPublisher = publisherForProperty(property)
         .first()
         .map {
-            Log.d("VMDExtensions", "propertyFlow map with it: $it")
             VMDAnimatedPropertyChange(it, VMDPropertyChange(property = property, oldValue = it, newValue = it))
         }
 
     val propertyChangePublisher =  propertyDidChange
         .filter {
-            Log.d("VMDExtensions", "propertyChangeFlow filter with it: $it")
             it.property.name == property.name
         }
         .map {
-            Log.d("VMDExtensions", "propertyChangeFlow map with it: $it")
             VMDAnimatedPropertyChange(value = it.newValue as T, propertyChange = it as VMDPropertyChange<T>)
         }
 
