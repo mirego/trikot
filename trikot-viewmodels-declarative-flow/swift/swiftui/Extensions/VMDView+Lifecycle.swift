07a80356a2e69a4deb78dd6651bdfdd51ed7aa12
Guillaume Bourassa <gbourassa@mirego.com>
2022-11-07 09:38:19 -0500

    [VMD-FLOW] Improved handling of lifecycle on both android and iOS

--- trikot-viewmodels-declarative-flow/swift/swiftui/Extensions/VMDView+Lifecycle.swift
+++ trikot-viewmodels-declarative-flow/swift/swiftui/Extensions/VMDView+Lifecycle.swift
@@ -1,11 +1,44 @@
 import SwiftUI
 import TRIKOT_FRAMEWORK_NAME
 
-extension View {
-    @ViewBuilder
-    func handleLifecycle(_ viewModel: VMDLifecycleViewModel) -> some View {
-        self
-            .onAppear { viewModel.onAppear() }
-            .onDisappear { viewModel.onDisappear() }
+struct LifecycleModifier: ViewModifier {
+
+    @Environment(\.scenePhase) var scenePhase
+
+    let viewModel: VMDLifecycleViewModel
+
+    @State private var skipFirstScenePhase = true
+    @State private var isDisplayed = false
+
+    func body(content: Content) -> some View {
+        content
+            .onChange(of: scenePhase) { newPhase in
+                if skipFirstScenePhase {
+                    skipFirstScenePhase = false
+                } else {
+                    if isDisplayed {
+                        if newPhase == .active {
+                            viewModel.onAppear()
+                        } else if newPhase == .background {
+                            viewModel.onDisappear()
+                        }
+                    }
+                }
+            }
+            .onAppear {
+                isDisplayed = true
+                viewModel.onAppear()
+            }
+            .onDisappear {
+                isDisplayed = false
+                viewModel.onDisappear()
+            }
     }
 }
+
+public extension View {
+    func handleLifecycle(_ viewModel: VMDLifecycleViewModel) -> some View {
+        modifier(LifecycleModifier(viewModel: viewModel))
+    }
+}
+
