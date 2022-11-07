c5de8786add0495badaec33f485d092965cc8fab
Guillaume Bourassa <gbourassa@mirego.com>
2022-11-07 13:34:23 -0500

    [VMD-Compiler] Added unit tests for VMD Compiler

--- trikot-viewmodels-declarative-compiler/viewmodels-declarative-compiler-flow/build.gradle.kts
+++ trikot-viewmodels-declarative-compiler/viewmodels-declarative-compiler-flow/build.gradle.kts
@@ -14,6 +14,11 @@ dependencies {
     implementation(project(Project.TRIKOT_VIEWMODELS_DECLARATIVE_COMPILER_CORE))
     implementation(project(Project.TRIKOT_VIEWMODELS_DECLARATIVE_FLOW))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.KOTLINX_COROUTINES}")
+
+    testImplementation("com.github.tschuchortdev:kotlin-compile-testing-ksp:1.4.9")
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+    testImplementation("org.jetbrains.kotlin:kotlin-reflect:${Versions.KOTLIN}")
 }
 
 tasks {
---
+++ trikot-viewmodels-declarative-compiler/viewmodels-declarative-compiler-flow/src/test/kotlin/com/mirego/trikot/viewmodels/declarative/compiler/TestReflectionUtil.kt
@@ -0,0 +1,55 @@
+package com.mirego.trikot.viewmodels.declarative.compiler
+
+import kotlin.reflect.KClass
+import kotlin.reflect.KFunction
+import kotlin.reflect.KParameter
+import kotlin.reflect.KTypeProjection
+import kotlin.reflect.full.createType
+import kotlin.reflect.full.functions
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+import kotlin.test.fail
+
+fun KParameter.assertParam(expectedName: String, expectedType: KClass<*>) {
+    assertEquals(expectedName, name)
+    assertEquals(expectedType.createType(), type)
+}
+
+fun KParameter.assertParam(expectedName: String, expectedType: KClass<*>, expectedTypeArgument: KClass<*>) {
+    assertEquals(expectedName, name)
+    assertEquals(expectedType.createType(listOf(KTypeProjection.invariant(expectedTypeArgument.createType()))), type)
+}
+
+fun KClass<*>.assertMember(name: String, type: KClass<*>) {
+    assertTrue(members.filter {
+        it.name == (name) &&
+            it.returnType == type.createType()
+    }.size == 1)
+}
+
+fun KClass<*>.assertMember(name: String, type: KClass<*>, typeArgument: KClass<*>) {
+    assertTrue(members.filter {
+        it.name == (name) &&
+            it.returnType == type.createType(listOf(KTypeProjection.invariant(typeArgument.createType())))
+    }.size == 1)
+}
+
+fun KClass<*>.getMandatoryFunction(name: String): KFunction<*> =
+    functions.firstOrNull {
+        it.name == name
+    }!!
+
+fun <R> KFunction<R>.assertParam(expectedName: String, expectedType: KClass<*>, expectedTypeParameter: KClass<*>) {
+    val parameter = parameters.firstOrNull {
+        it.name == expectedName
+    } ?: fail("Parameter not found $expectedName")
+    parameter.assertParam(expectedName, expectedType, expectedTypeParameter)
+}
+
+fun <R> KFunction<R>.assertReturnType(type: KClass<*>) {
+    assertEquals(type.createType(), returnType)
+}
+
+fun <R> KFunction<R>.assertReturnType(type: KClass<*>, typeParameter: KClass<*>) {
+    assertEquals(type.createType(listOf(KTypeProjection.invariant(typeParameter.createType()))), returnType)
+}
