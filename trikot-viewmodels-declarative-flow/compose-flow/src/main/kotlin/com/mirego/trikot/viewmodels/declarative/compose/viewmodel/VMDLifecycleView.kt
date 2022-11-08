0be6426c0d8bae864882668880234007d563acb6
Guillaume Bourassa <gbourassa@mirego.com>
2022-11-08 10:25:22 -0500

    [VMD-FLOW] Improved handling of lifecycle on both android and iOS (#111)

--- trikot-viewmodels-declarative-flow/compose-flow/src/main/kotlin/com/mirego/trikot/viewmodels/declarative/compose/viewmodel/VMDLifecycleView.kt
+++ trikot-viewmodels-declarative-flow/compose-flow/src/main/kotlin/com/mirego/trikot/viewmodels/declarative/compose/viewmodel/VMDLifecycleView.kt
@@ -2,25 +2,37 @@ package com.mirego.trikot.viewmodels.declarative.compose.viewmodel
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import com.mirego.trikot.viewmodels.declarative.viewmodel.VMDLifecycleViewModel
-import kotlinx.coroutines.CoroutineScope
 
+/**
+ * For views backed by a VMDLifecycleViewModel, wrap your content with VMDLifecycleView to properly bind the lifecycle.
+ */
 @Composable
 fun VMDLifecycleView(
-    viewModel: VMDLifecycleViewModel,
-    preOnAppear: suspend (CoroutineScope) -> Unit = {},
+    lifecycleViewModel: VMDLifecycleViewModel,
     content: @Composable () -> Unit
 ) {
+    val lifecycleOwner by rememberUpdatedState(LocalLifecycleOwner.current)
     content()
-    LaunchedEffect(LocalLifecycleOwner.current) {
-        preOnAppear(this)
-        viewModel.onAppear()
-    }
-    DisposableEffect(LocalLifecycleOwner.current) {
+    DisposableEffect(lifecycleOwner) {
+        val observer = LifecycleEventObserver { _, event ->
+            if (event == Lifecycle.Event.ON_START) {
+                lifecycleViewModel.onAppear()
+            } else if (event == Lifecycle.Event.ON_STOP) {
+                lifecycleViewModel.onDisappear()
+            }
+        }
+        lifecycleOwner.lifecycle.addObserver(observer)
         onDispose {
-            viewModel.onDisappear()
+            lifecycleOwner.lifecycle.removeObserver(observer)
+            if (lifecycleOwner.lifecycle.currentState.isAtLeast(Lifecycle.State.STARTED)) {
+                lifecycleViewModel.onDisappear()
+            }
         }
     }
 }
