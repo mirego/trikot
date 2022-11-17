6dcd139255a9b5e05569f870d01e9acd4d867c21
Nicolas Presseault <npresseault@mirego.com>
2022-11-17 09:55:33 -0500

    Bind hidden

--- trikot-viewmodels-declarative/compose/src/main/kotlin/com/mirego/trikot/viewmodels/declarative/compose/viewmodel/VMDDropDownMenu.kt
+++ trikot-viewmodels-declarative/compose/src/main/kotlin/com/mirego/trikot/viewmodels/declarative/compose/viewmodel/VMDDropDownMenu.kt
@@ -16,6 +16,8 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.PopupProperties
 import com.mirego.trikot.viewmodels.declarative.components.VMDPickerItemViewModel
 import com.mirego.trikot.viewmodels.declarative.components.VMDPickerViewModel
+import com.mirego.trikot.viewmodels.declarative.compose.extensions.hidden
+import com.mirego.trikot.viewmodels.declarative.compose.extensions.isOverridingAlpha
 import com.mirego.trikot.viewmodels.declarative.compose.extensions.observeAsState
 
 @Composable
@@ -28,10 +30,17 @@ fun <E : VMDPickerItemViewModel> VMDDropDownMenu(
     properties: PopupProperties = PopupProperties(focusable = true),
     content: @Composable ColumnScope.(item: E, index: Int) -> Unit
 ) {
-    val pickerViewModel by viewModel.observeAsState()
-    if (pickerViewModel.isHidden) return
+    val pickerViewModel by viewModel.observeAsState(excludedProperties = if (modifier.isOverridingAlpha()) listOf(viewModel::isHidden) else emptyList())
 
-    DropdownMenu(expanded, onDismissRequest, modifier, offset, properties) {
+    DropdownMenu(
+        expanded = expanded,
+        onDismissRequest = onDismissRequest,
+        modifier = Modifier
+            .hidden(pickerViewModel.isHidden)
+            .then(modifier),
+        offset = offset,
+        properties = properties
+    ) {
         pickerViewModel.elements.forEachIndexed { index, item ->
             content(item, index)
         }
@@ -49,15 +58,16 @@ fun <E : VMDPickerItemViewModel> VMDDropDownMenuItem(
     interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
     content: @Composable RowScope.(item: E, index: Int) -> Unit
 ) {
-    val pickerItemViewModel by viewModel.observeAsState()
-    if (pickerItemViewModel.isHidden) return
+    val pickerItemViewModel by viewModel.observeAsState(excludedProperties = if (modifier.isOverridingAlpha()) listOf(viewModel::isHidden) else emptyList())
 
     DropdownMenuItem(
         onClick = {
             pickerViewModel.selectedIndex = index
             onClick()
         },
-        modifier = modifier,
+        modifier = Modifier
+            .hidden(pickerItemViewModel.isHidden)
+            .then(modifier),
         enabled = pickerItemViewModel.isEnabled,
         contentPadding = contentPadding,
         interactionSource = interactionSource
