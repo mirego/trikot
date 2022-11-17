073c5623d47b19bfb1c976f4b857b5f7a6e30a82
Mathieu Larue <mlarue@mirego.com>
2022-11-17 14:40:01 -0500

    Flow

---
+++ trikot-viewmodels-declarative-flow/viewmodels-declarative-flow/src/commonMain/kotlin/com/mirego/trikot/viewmodels/declarative/viewmodel/VMDViewModelDSL.kt
@@ -0,0 +1,143 @@
+package com.mirego.trikot.viewmodels.declarative.viewmodel
+
+import com.mirego.trikot.viewmodels.declarative.components.factory.VMDComponents
+import com.mirego.trikot.viewmodels.declarative.components.impl.VMDButtonViewModelImpl
+import com.mirego.trikot.viewmodels.declarative.components.impl.VMDImageViewModelImpl
+import com.mirego.trikot.viewmodels.declarative.components.impl.VMDListViewModelImpl
+import com.mirego.trikot.viewmodels.declarative.components.impl.VMDLoadingViewModelImpl
+import com.mirego.trikot.viewmodels.declarative.components.impl.VMDProgressViewModelImpl
+import com.mirego.trikot.viewmodels.declarative.components.impl.VMDTextFieldViewModelImpl
+import com.mirego.trikot.viewmodels.declarative.components.impl.VMDTextViewModelImpl
+import com.mirego.trikot.viewmodels.declarative.components.impl.VMDToggleViewModelImpl
+import com.mirego.trikot.viewmodels.declarative.content.VMDIdentifiableContent
+import com.mirego.trikot.viewmodels.declarative.content.VMDImageContent
+import com.mirego.trikot.viewmodels.declarative.content.VMDImageDescriptorContent
+import com.mirego.trikot.viewmodels.declarative.content.VMDNoContent
+import com.mirego.trikot.viewmodels.declarative.content.VMDTextContent
+import com.mirego.trikot.viewmodels.declarative.content.VMDTextImagePairContent
+import com.mirego.trikot.viewmodels.declarative.content.VMDTextPairContent
+import com.mirego.trikot.viewmodels.declarative.properties.VMDImageDescriptor
+import com.mirego.trikot.viewmodels.declarative.properties.VMDImageResource
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+interface VMDViewModelDSL {
+    val coroutineScope: CoroutineScope
+}
+
+fun VMDViewModelDSL.text(content: String = "", closure: VMDTextViewModelImpl.() -> Unit = {}) =
+    VMDComponents.Text.withContent(content, coroutineScope, closure)
+
+fun VMDViewModelDSL.localImage(image: VMDImageResource = VMDImageResource.None, closure: VMDImageViewModelImpl.() -> Unit = {}) =
+    VMDComponents.Image.local(image, coroutineScope, closure)
+
+fun VMDViewModelDSL.remoteImage(imageUrl: String? = null, placeholderImageResource: VMDImageResource = VMDImageResource.None, closure: VMDImageViewModelImpl.() -> Unit = {}) =
+    VMDComponents.Image.remote(imageUrl, placeholderImageResource, coroutineScope, closure)
+
+fun VMDViewModelDSL.image(imageDescriptor: VMDImageDescriptor = VMDImageDescriptor.Local(VMDImageResource.None), closure: VMDImageViewModelImpl.() -> Unit = {}) =
+    VMDComponents.Image.withDescriptor(imageDescriptor, coroutineScope, closure)
+
+fun VMDViewModelDSL.button(closure: VMDButtonViewModelImpl<VMDNoContent>.() -> Unit = {}) =
+    VMDComponents.Button.empty(coroutineScope, closure)
+
+fun VMDViewModelDSL.buttonWithText(text: String = "", action: () -> Unit = {}, closure: VMDButtonViewModelImpl<VMDTextContent>.() -> Unit = {}) =
+    VMDComponents.Button.withText(text, coroutineScope, closure).apply {
+        setAction(action)
+    }
+
+fun VMDViewModelDSL.buttonWithImage(image: VMDImageResource = VMDImageResource.None, action: () -> Unit = {}, closure: VMDButtonViewModelImpl<VMDImageContent>.() -> Unit = {}) =
+    VMDComponents.Button.withImage(image, coroutineScope, closure).apply {
+        setAction(action)
+    }
+
+fun VMDViewModelDSL.buttonWithImage(
+    initialImage: VMDImageResource,
+    image: Flow<VMDImageResource>,
+    action: () -> Unit,
+    closure: VMDButtonViewModelImpl<VMDImageContent>.() -> Unit = {}
+) =
+    VMDComponents.Button.withImage(initialImage, coroutineScope, closure).apply {
+        bindContent(image.map { VMDImageContent(it) })
+        setAction(action)
+    }
+
+fun VMDViewModelDSL.buttonWithImageUrl(
+    imageUrl: String = "",
+    placeholderImageResource: VMDImageResource = VMDImageResource.None,
+    action: () -> Unit = {},
+    closure: VMDButtonViewModelImpl<VMDImageDescriptorContent>.() -> Unit = {}
+) =
+    VMDComponents.Button.withImageUrl(imageUrl, placeholderImageResource, coroutineScope, closure).apply {
+        setAction(action)
+    }
+
+fun VMDViewModelDSL.buttonWithTextPair(first: String = "", second: String = "", action: () -> Unit = {}, closure: VMDButtonViewModelImpl<VMDTextPairContent>.() -> Unit = {}) =
+    VMDComponents.Button.withTextPair(
+        VMDTextPairContent(
+            first,
+            second
+        ),
+        coroutineScope,
+        closure
+    ).apply {
+        setAction(action)
+    }
+
+fun VMDViewModelDSL.buttonWithTextImage(
+    text: String = "",
+    image: VMDImageResource = VMDImageResource.None,
+    action: () -> Unit = {},
+    closure: VMDButtonViewModelImpl<VMDTextImagePairContent>.() -> Unit = {}
+) =
+    VMDComponents.Button.withTextImage(
+        VMDTextImagePairContent(
+            text,
+            image
+        ),
+        coroutineScope,
+        closure
+    ).apply {
+        setAction(action)
+    }
+
+fun VMDViewModelDSL.textField(text: String = "", placeholder: String = "", closure: VMDTextFieldViewModelImpl.() -> Unit = {}) = VMDComponents.TextField.empty(coroutineScope, closure).apply {
+    this.text = text
+    this.placeholder = placeholder
+}
+
+fun VMDViewModelDSL.toggleWithText(text: String = "", isOn: Boolean = false, closure: VMDToggleViewModelImpl<VMDTextContent>.() -> Unit = {}) =
+    VMDComponents.Toggle.withText(text, isOn, coroutineScope, closure)
+
+fun VMDViewModelDSL.loading(isLoading: Boolean = false, closure: VMDLoadingViewModelImpl.() -> Unit = {}) = VMDLoadingViewModelImpl(coroutineScope)
+    .apply {
+        this.isLoading = isLoading
+        closure()
+    }
+
+fun VMDViewModelDSL.loading(isLoading: Flow<Boolean>, closure: VMDLoadingViewModelImpl.() -> Unit = {}) = VMDLoadingViewModelImpl(coroutineScope)
+    .apply {
+        bindIsLoading(isLoading)
+        closure()
+    }
+
+fun VMDViewModelDSL.indeterminateProgress(closure: VMDProgressViewModelImpl.() -> Unit = {}) =
+    VMDComponents.Progress.indeterminate(coroutineScope, closure)
+
+fun VMDViewModelDSL.determinateProgress(progress: Float = 0f, total: Float = 1f, closure: VMDProgressViewModelImpl.() -> Unit = {}) =
+    VMDComponents.Progress.determinate(progress, total, coroutineScope, closure)
+
+fun <C : VMDIdentifiableContent> VMDViewModelDSL.list(elements: List<C> = emptyList(), closure: VMDListViewModelImpl<C>.() -> Unit = {}) =
+    VMDComponents.List.empty(coroutineScope, closure).apply {
+        this.elements = elements
+    }
+
+fun <C : VMDIdentifiableContent> VMDViewModelDSL.list(vararg elements: C, closure: VMDListViewModelImpl<C>.() -> Unit = {}) =
+    VMDComponents.List.empty(coroutineScope, closure).apply {
+        this.elements = elements.toList()
+    }
+
+fun <C : VMDIdentifiableContent> VMDViewModelDSL.list(elements: Flow<List<C>>, closure: VMDListViewModelImpl<C>.() -> Unit = {}) =
+    VMDComponents.List.empty(coroutineScope, closure).apply {
+        bindElements(elements)
+    }
