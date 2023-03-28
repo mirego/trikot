dcbb6c1478e96a1cdd53b5808116d72cc67c637e
Laurent Boileau <lboileau@mirego.com>
2023-03-28 15:39:22 -0400

    Display plain text when using iOS 13, 14

--- trikot-viewmodels-declarative-flow/swift/swiftui/Components/VMDText.swift
+++ trikot-viewmodels-declarative-flow/swift/swiftui/Components/VMDText.swift
@@ -17,44 +17,27 @@ public struct VMDText: View {
     }
 
     public var body: some View {
-        Group {
-            if viewModel.spans.isEmpty {
-                configurations.reduce(Text(viewModel.text)) { current, config in
-                    config(current)
-                }
-            } else {
-                if #available(iOS 15, *) {
-                    Text(AttributedString(text: viewModel.text, spans: viewModel.spans))
-                } else {
-                    UIRichText(observableViewModel: observableViewModel)
-                }
-            }
+        configurations.reduce(text) { current, config in
+            config(current)
         }
         .hidden(viewModel.isHidden)
+    }
+
+    private var text: Text {
+        if viewModel.spans.isEmpty {
+            return Text(viewModel.text)
+        } else {
+            if #available(iOS 15, *) {
+                return Text(AttributedString(text: viewModel.text, spans: viewModel.spans))
+            } else {
+                return Text(viewModel.text)
+            }
+        }
     }
 
     public func configure(_ block: @escaping TextConfiguration) -> VMDText {
         var result = self
         result.configurations.append(block)
         return result
-    }
-}
-
-@available(iOS, obsoleted: 15, message: "Use Text.init(_ attributedContent: AttributedString)")
-private struct UIRichText: UIViewRepresentable {
-    @ObservedObject var observableViewModel: ObservableViewModelAdapter<VMDTextViewModel>
-
-    private var viewModel: VMDTextViewModel {
-        observableViewModel.viewModel
-    }
-
-    func makeUIView(context: Context) -> UILabel {
-        let label = UILabel()
-        label.attributedText = NSAttributedString(text: viewModel.text, spans: viewModel.spans)
-        return label
-    }
-
-    func updateUIView(_ label: UILabel, context: Context) {
-        label.attributedText = NSAttributedString(text: viewModel.text, spans: viewModel.spans)
     }
 }
--- trikot-viewmodels-declarative-flow/swift/swiftui/SpanStyleResource/VMDSpanStyleProvider.swift
+++ trikot-viewmodels-declarative-flow/swift/swiftui/SpanStyleResource/VMDSpanStyleProvider.swift
@@ -10,14 +10,6 @@ public protocol VMDSpanStyleProvider {
 @available(iOS 15, *)
 internal extension AttributedString {
     init(text: String, spans: [VMDRichTextSpan]) {
-        self.init(
-            NSAttributedString(text: text, spans: spans)
-        )
-    }
-}
-
-internal extension NSAttributedString {
-    convenience init(text: String, spans: [VMDRichTextSpan]) {
         let attributedString = NSMutableAttributedString(string: text)
         for span in spans {
             guard
@@ -30,6 +22,6 @@ internal extension NSAttributedString {
             let range = NSRange(location: start, length: length)
             attributedString.addAttributes(spanStyle, range: range)
         }
-        self.init(attributedString: attributedString)
+        self.init(attributedString)
     }
 }
