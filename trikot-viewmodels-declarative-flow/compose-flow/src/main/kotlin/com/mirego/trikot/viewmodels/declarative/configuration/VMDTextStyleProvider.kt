15af7fd034b1e45a9d885d2e9d6d1bd398ec60a4
Nicolas Langheit <35898659+nlangheit@users.noreply.github.com>
2023-03-28 10:29:58 -0400

    Rich Text Support [Android] (#153)
    * Add rich text implementation for android
    
    * PR comments
    
    * Add dummy image and style providers
    
    * PR comments
    
    * Fix CI check
    
    * Pr comments

---
+++ trikot-viewmodels-declarative-flow/compose-flow/src/main/kotlin/com/mirego/trikot/viewmodels/declarative/configuration/VMDTextStyleProvider.kt
@@ -0,0 +1,12 @@
+package com.mirego.trikot.viewmodels.declarative.configuration
+
+import androidx.compose.ui.text.TextStyle
+import com.mirego.trikot.viewmodels.declarative.properties.VMDTextStyleResource
+
+interface VMDTextStyleProvider {
+    fun textStyleForResource(resource: VMDTextStyleResource): TextStyle?
+}
+
+class DefaultTextStyleProvider: VMDTextStyleProvider {
+    override fun textStyleForResource(resource: VMDTextStyleResource): TextStyle? = null
+}
