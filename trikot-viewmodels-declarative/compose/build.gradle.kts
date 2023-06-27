335c0d5c8c9bcfa2b0e07eeb15eaa79ffa2d1cc3
Steve Matte <smatte@mirego.com>
2023-06-27 08:42:33 -0400

    Code review: Use kotlin test

--- trikot-viewmodels-declarative/compose/build.gradle.kts
+++ trikot-viewmodels-declarative/compose/build.gradle.kts
@@ -48,8 +48,7 @@ dependencies {
     api("androidx.compose.ui:ui-tooling:${Versions.JETPACK_COMPOSE_UI_TOOLING}")
     api("io.coil-kt:coil-compose:${Versions.COIL}")
 
-    implementation("org.jetbrains.kotlin:kotlin-test")
-    implementation("org.jetbrains.kotlin:kotlin-test-junit")
+    testImplementation(kotlin("test"))
 
     implementation("org.jetbrains.kotlin:kotlin-reflect:${Versions.KOTLIN}")
 }
