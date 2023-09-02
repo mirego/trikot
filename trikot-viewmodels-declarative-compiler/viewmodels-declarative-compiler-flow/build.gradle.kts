255b80060cb6e4527999ea0aed9ddb23f48a596a
Nicolas Presseault <npresseault@mirego.com>
2023-09-02 19:26:07 -0400

    bump compile testing version

--- trikot-viewmodels-declarative-compiler/viewmodels-declarative-compiler-flow/build.gradle.kts
+++ trikot-viewmodels-declarative-compiler/viewmodels-declarative-compiler-flow/build.gradle.kts
@@ -14,7 +14,7 @@ dependencies {
     implementation(project(Project.TRIKOT_VIEWMODELS_DECLARATIVE_FLOW))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.KOTLINX_COROUTINES}")
 
-    testImplementation("com.github.tschuchortdev:kotlin-compile-testing-ksp:1.4.9")
+    testImplementation("com.github.tschuchortdev:kotlin-compile-testing-ksp:1.5.0")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
     testImplementation("org.jetbrains.kotlin:kotlin-reflect:${Versions.KOTLIN}")
