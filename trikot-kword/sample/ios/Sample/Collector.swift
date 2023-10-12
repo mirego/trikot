f77f235e6dc8e91faf9b040eb3255f9c3837a7c2
Nicolas Presseault <npresseault@mirego.com>
2023-10-12 09:58:14 -0400

    Fix kword sample, FlowCollector signature changed in latest kotlinx.coroutines version.

--- trikot-kword/sample/ios/Sample/Collector.swift
+++ trikot-kword/sample/ios/Sample/Collector.swift
@@ -1,17 +1,15 @@
 import TRIKOT_FRAMEWORK_NAME
 
 class Collector<T>: FlowCollector {
-    
     let callback:(T) -> Void
 
     init(callback: @escaping (T) -> Void) {
         self.callback = callback
     }
-
-
-    func emit(value: Any?, completionHandler: @escaping (KotlinUnit?, Error?) -> Void) {
+    
+    func emit(value: Any?, completionHandler: @escaping (Error?) -> Void) {
         callback(value as! T)
 
-        completionHandler(KotlinUnit(), nil)
+        completionHandler(nil)
     }
 }
