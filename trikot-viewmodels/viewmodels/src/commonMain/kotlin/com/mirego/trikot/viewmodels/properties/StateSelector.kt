4bc5ad66097d66b72ea916e091e4145c7c483704
Philippe Lamy <plamy@mirego.com>
2023-11-07 13:53:30 -0500

    Rename default state

--- trikot-viewmodels/viewmodels/src/commonMain/kotlin/com/mirego/trikot/viewmodels/properties/StateSelector.kt
+++ trikot-viewmodels/viewmodels/src/commonMain/kotlin/com/mirego/trikot/viewmodels/properties/StateSelector.kt
@@ -1,10 +1,10 @@
 package com.mirego.trikot.viewmodels.properties
 
 data class StateSelector<T>(
-    val default: T? = null,
+    val defaultState: T? = null,
     val highlighted: T? = null,
     val selected: T? = null,
     val disabled: T? = null
 ) {
-    val isEmpty: Boolean get() = default == null && highlighted == null && selected == null && disabled == null
+    val isEmpty: Boolean get() = defaultState == null && highlighted == null && selected == null && disabled == null
 }
