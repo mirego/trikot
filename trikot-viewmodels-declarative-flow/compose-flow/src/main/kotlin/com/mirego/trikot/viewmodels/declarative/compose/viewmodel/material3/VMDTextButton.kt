dcd41d765cc471b910534a38b003950ebd3e9fa6
Mathieu Larue <mlarue@mirego.com>
2023-11-19 11:44:07 -0500

    Fix

--- trikot-viewmodels-declarative-flow/compose-flow/src/main/kotlin/com/mirego/trikot/viewmodels/declarative/compose/viewmodel/material3/VMDTextButton.kt
+++ trikot-viewmodels-declarative-flow/compose-flow/src/main/kotlin/com/mirego/trikot/viewmodels/declarative/compose/viewmodel/material3/VMDTextButton.kt
@@ -1,12 +1,18 @@
 package com.mirego.trikot.viewmodels.declarative.compose.viewmodel.material3
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ButtonElevation
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
 import com.mirego.trikot.viewmodels.declarative.components.VMDButtonViewModel
 import com.mirego.trikot.viewmodels.declarative.compose.extensions.hidden
 import com.mirego.trikot.viewmodels.declarative.compose.extensions.isOverridingAlpha
@@ -17,6 +23,11 @@ import com.mirego.trikot.viewmodels.declarative.content.VMDContent
 fun <C : VMDContent> VMDTextButton(
     viewModel: VMDButtonViewModel<C>,
     modifier: Modifier = Modifier,
+    shape: Shape = ButtonDefaults.textShape,
+    colors: ButtonColors = ButtonDefaults.textButtonColors(),
+    elevation: ButtonElevation? = null,
+    border: BorderStroke? = null,
+    contentPadding: PaddingValues = ButtonDefaults.TextButtonContentPadding,
     interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
     content: @Composable (RowScope.(field: C) -> Unit)
 ) {
@@ -28,6 +39,11 @@ fun <C : VMDContent> VMDTextButton(
             .hidden(buttonViewModel.isHidden)
             .then(modifier),
         enabled = buttonViewModel.isEnabled,
+        shape = shape,
+        colors = colors,
+        elevation = elevation,
+        border = border,
+        contentPadding = contentPadding,
         interactionSource = interactionSource
     ) {
         content(buttonViewModel.content)
