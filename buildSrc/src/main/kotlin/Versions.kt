890f50b8c9407e4b2ba66d9ace967d29e72b7b7c
Nicolas Presseault <npresseault@mirego.com>
2023-12-07 11:43:10 -0500

    Bump coil to 2.5.0

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -6,7 +6,7 @@ object Versions {
     const val JETPACK_COMPOSE_RUNTIME = "1.5.0"
     const val JETPACK_COMPOSE_MATERIAL_3 = "1.1.1"
     const val JETPACK_COMPOSE_TV = "1.0.0-alpha10"
-    const val COIL = "2.0.0-rc03"
+    const val COIL = "2.5.0"
     const val KTLINT = "11.6.1"
     const val KOTLINX_SERIALIZATION = "1.6.0"
     const val KOTLINX_COROUTINES = "1.7.3"
