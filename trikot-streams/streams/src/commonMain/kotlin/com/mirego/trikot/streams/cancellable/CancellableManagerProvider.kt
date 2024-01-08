52b09e34edf08595344db1dd863afda1a62b9780
Marc Lefrançois <mlefrancois@mirego.com>
2024-01-08 12:53:56 -0500

    Fix forever growing cancellable Manager
    A long lived CancellableManagerProvider could easily end up with very many “cancelled” Cancellable in it’s internal CancellableManager because this is only cleared when the Provider itself is cancelled.
    
    This can cause huge memory leaks in some cases, for example in the DebounceProcessor
    
    Since there is only ever one “not cancelled” CancellableManager provided by the CancellableManagerProvider, there is actually no need to keep the previously provided, now cancelled, CancellableManager.
    
    We now simply use the internalCancellableManagerRef.
    
    Also took the oportunity to make cancelPreviousAndCreate hopefully atomic be exposing and using getAndSet on AtomicReference

--- trikot-streams/streams/src/commonMain/kotlin/com/mirego/trikot/streams/cancellable/CancellableManagerProvider.kt
+++ trikot-streams/streams/src/commonMain/kotlin/com/mirego/trikot/streams/cancellable/CancellableManagerProvider.kt
@@ -3,18 +3,14 @@ package com.mirego.trikot.streams.cancellable
 import com.mirego.trikot.foundation.concurrent.AtomicReference
 
 class CancellableManagerProvider : Cancellable {
-    private val cancellableManager = CancellableManager()
     private val internalCancellableManagerRef = AtomicReference(CancellableManager())
 
-    fun cancelPreviousAndCreate(): CancellableManager {
-        internalCancellableManagerRef.value.cancel()
-        return CancellableManager().also {
-            internalCancellableManagerRef.setOrThrow(internalCancellableManagerRef.value, it)
-            cancellableManager.add(it)
+    fun cancelPreviousAndCreate(): CancellableManager =
+        CancellableManager().also {
+            internalCancellableManagerRef.getAndSet(it).cancel()
         }
-    }
 
     override fun cancel() {
-        cancellableManager.cancel()
+        internalCancellableManagerRef.value.cancel()
     }
 }
