38473eb8eabcd649f6e1bac61a45bde1aaf51da8
Nicolas Presseault <npresseault@mirego.com>
2024-08-01 11:32:17 -0400

    No need for regex here

--- build.gradle.kts
+++ build.gradle.kts
@@ -76,7 +76,7 @@ tasks {
         destinationFile.set(file("${rootDir}/gradle.properties"))
         properties(java.util.Properties().apply { load(destinationFile.asFile.get().reader()) }.mapKeys { it.key.toString() })
         val gitCommits = "git rev-list --count HEAD".runCommand(workingDir = rootDir)
-        val originalVersion = project.version.toString().replace("-dev\\w+".toRegex(), "").replace("-SNAPSHOT".toRegex(), "")
+        val originalVersion = project.version.toString().replace("-dev\\w+".toRegex(), "").replace("-SNAPSHOT", "")
         property("version", "$originalVersion-dev$gitCommits")
     }
     val tagVersion by registering(TagVersionTask::class)
