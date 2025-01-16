47613d722df050f0c88e5b8874bf10f705705739
Charles MacDuff <cmacduff@mirego.com>
2025-01-16 13:31:05 -0500

    Support multiple translation files

--- trikot-kword/kword-plugin/src/main/kotlin/com.mirego.kword/KWordExtension.kt
+++ trikot-kword/kword-plugin/src/main/kotlin/com.mirego.kword/KWordExtension.kt
@@ -2,12 +2,9 @@ package com.mirego.kword
 
 import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.file.DirectoryProperty
-import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.provider.Property
 
 abstract class KWordExtension {
-    abstract val translationFile: RegularFileProperty
-
     abstract val translationFiles: ConfigurableFileCollection
 
     abstract val enumClassName: Property<String>
